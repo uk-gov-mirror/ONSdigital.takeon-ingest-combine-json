5a269bca69b9841749042f31922e63032e6732ea
RyanAldy <ryanalderson91@gmail.com>
2019-12-12 11:11:35 +0000

    Removed queue being instantiated in error queue function

--- write_to_queue.py
+++ write_to_queue.py
@@ -15,7 +15,6 @@ def send_outcomes_to_queue(queue_url, data):
 
 def send_to_error_queue(queue_url, errorMessage):
     try:
-        queue_url = os.getenv("ERROR_QUEUE_URL")
         msg = sqs_client.send_message(QueueUrl=queue_url, MessageBody=errorMessage)
     except ClientError as e:
         print("Error in sending to queue: " + e)
